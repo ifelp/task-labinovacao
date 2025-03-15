05ec02e1affbb4ebc856bed6301145540f3367db
Iranildo <iranildofpersonal18@gmail.com>
2025-03-15 15:52:16 -0300

    adicionando servidor bun com novas rotas e arquivos

--- bun-server/src/server.ts
+++ bun-server/src/server.ts
@@ -2,11 +2,14 @@ import { serve } from "bun";
 
 const app = Bun.serve
 
+const html = await Bun.file("./src/example.html").text()
+
 app({
     port: 3000,
     routes:{
+
         "/" : Response.json({message: "Server rodando na porta 3000"}, {status: 200}),
-        //rotas com fotos (retirar depois)
+
         "/citi": new Response(await Bun.file("./src/assets/Logo.png").bytes(), {
             headers: {
                 "Content-Type": "image"
@@ -19,17 +22,22 @@ app({
         }),
 
         "/citi/reqs":{
-            GET: () => new Response("Tudo certo por aqui", {status: 200}),
-            POST: async req =>{
-                const body = await req.json()
-                return Response.json({message: `Olá, ${body.name}`},{created: true, ...body}) //isso supondo que você irá colocar uma propriedade name no seu body
-            },
-            PUT: async req=>{
-                const body = await req.json()
-                return Response.json({updated: true, ...body})
-            },
-            DELETE: ()=> new Response("Deletado", {status: 204})
-        }
+            GET: () => new Response("Tudo certo por aqui.", {status: 200}),
+            POST: ()=> Response.json({message: 'criado.', created: true}),
+            PUT: ()=> Response.json({message:'atualizado.', updated:true}),
+            DELETE: ()=> new Response('', {status: 204})
+        },
+
+        "/citi/lab-inovacao" : new Response(html, {
+            headers:{
+            "Content-Type": "text/html"
+        }}),
+
+        "/citi/dev": Response.json({message: "Desenvolvimento 25.1"}, {status: 200}),
+
+        "/citi/ditto": new Response(await Bun.file("./src/assets/Ditto.webp").bytes()),
+
+        "/citi/skylines": new Response(await Bun.file("./src/assets/cities-skylines.jpg").bytes())
     },
     error(error){
         console.error(error.message)
